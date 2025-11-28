d75609061e68f01dd99023ad005fe2a055885fa0
joao1133312 <joao.martins.barizon@escola.pr.gov.br>
2025-11-28 07:32:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+const scenes = document.querySelectorAll(".scene");
+const buttons = document.querySelectorAll("button[data-next]");
+
+buttons.forEach(btn => {
+    btn.addEventListener("click", () => {
+        const next = btn.dataset.next;
+
+        document.querySelector(".scene.active").classList.remove("active");
+        document.getElementById("scene-" + next).classList.add("active");
+    });
+});
 
